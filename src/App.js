a0c2dd9c84e390a994578b4668add75089640f9f
Nizar Mayraldo <nizar.aldo@yahoo.com>
2022-06-24 21:58:47 +0700

    m3s1 & m5s2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 import './App.css';
-import data from './example';
-import Isi from './Components/cardmusic';
+import Login from './Pages/Login';
+import Dashboard from './Pages/Dashboard';
+import { useEffect, useState } from 'react';
 
 function App() {
 
-  const clientid = process.env.REACT_APP_SPOTKEY;
-  console.log(clientid);
+  const [token, setToken] = useState("")
+
+  useEffect(  ()  =>  {
+
+    const hash = window.location.hash
+    let token = window.localStorage.getItem("token")
+
+    if(!token && hash){
+      token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+
+      window.location.hash =""
+      window.localStorage.setItem("token", token)
+      
+    }
+
+    setToken(token)
+  
+  }, [])
+
+  const logout = () =>{
+    setToken("")
+    window.localStorage.removeItem("token")
+  }
 
   return (
     <div className="App">
-       {data.map(item =>(
-          <Isi 
-            key={item.id}
-            size={item.album.images[0].height}
-            url={item.album.images[0].url}
-            judul={item.album.name}
-            nama={item.artists[0].name}
+       { token ?
+          <Dashboard 
+              token = {token}
+              logout = {logout}
           />
-        ))}
+        :
+          <Login />
+
+       }
     </div>
        
   );
---
+++ src/Functionals/Auth.js
@@ -0,0 +1,14 @@
+const client_id = process.env.REACT_APP_SPOTKEY
+const redirect_uri = "http://localhost:3000/"
+const AuthEndpoint = "https://accounts.spotify.com/authorize"
+const scope = "playlist-modify-private"
+// const scopes = [
+// "streaming",
+// "user-read-email",
+// "user-read-private",
+// ];
+
+// export const loginUrl = `${AuthEndpoint}?client_id=${client_id}&response_type=token&redirect_uri=${redirect_uri}&scope=${scopes.join("%20")}`;
+export const loginUrl = `${AuthEndpoint}?client_id=${client_id}&response_type=token&redirect_uri=${redirect_uri}&scope=${scope}`;
+  // loginUrl = "https://accounts.spotify.com/authorize?client_id=YourClientId&response_type=code&redirect_uri=https://localhost:3000/&scope=streaming%20user-read-email%20user-read-private"
+  
